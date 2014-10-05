93cdc72f7c0b48fe64cab6904cea21fcd9c3f01a
AidanCoward <aidan.coward@gmail.com>
2014-10-05 00:26:33 -0400

    removed files from repo since they were moved into the python/learn-python directory

--- ex12.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-
-pony, rhino, second, third, fifth = argv
-
-print "The script is called:", pony
-print "Your first variable is:", rhino
-print "Your second variable is:", second
-print "Your third variable is:", third
-print "your fourth variable is:", fifth
--- ex15.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-
-script, ftext = argv
-
-print "Here's your file %r:" % ftext
-gtext = open(ftext)
-htext = gtext
-print htext.read()
-
